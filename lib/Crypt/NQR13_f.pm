decc350419a7e5baf07a6bd8b0bda12fbeb2e70a
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2009-09-23 23:24:28 -0500

    more codes

--- lib/Crypt/NQR13_f.pm
+++ lib/Crypt/NQR13_f.pm
@@ -1,18 +1,36 @@
 package Crypt::NQR13_f;
 use signatures;
 use List::Util 'shuffle';
+use List::MoreUtils 'zip';
 use feature ':5.10';
+use utf8;
+use strict;
+use warnings;
 
-use constant {
-   START  => q{X},
-   MIDDLE => q{Y},
-   ENDING => q{Z},
-};
+sub generate_mapping {
+   my @alphabet = ('a'..'z');
+   my @random   = shuffle ('a'..'z');
+   return { zip @alphabet, @random };
+}
+
+sub tr_with_mapping ($mapping, $_) {
+   my $from = join q{}, keys %{$mapping};
+   my $to = join q{}, values %{$mapping};
+   eval "tr/$from/$to/";
+   $_;
+}
+
+sub untr_with_mapping ($mapping, $_) {
+   my $to = join q{}, keys %{$mapping};
+   my $from = join q{}, values %{$mapping};
+   eval "tr/$from/$to/";
+   $_;
+}
 
 sub encode_word ($word) {
    given (length $word) {
       when (0) {}
-      when (1) { $word = START.$word.ENDING; }
+      when (1) { $word = "X${word}Z"; }
       when (2) { $word =~ s/^(.)(.)$/$2Y$1/; } # boobs!
       default {
          $word =~ /^(.)(.*?)(.)?$/; # get first and last characters
@@ -23,8 +41,9 @@ sub encode_word ($word) {
 }
 
 sub encode ($message) {
-   my @words = map encode_word($_), split /\s/, $message;
-   return join q{ }, @words;
+   my @sentances =
+   my @words =  lc $message;
+   return join qq{\n}, map { join q{ }, map encode_word($_), split / /} split /\n/, lc $message;
 }
 
 sub decode_word ($word) {
@@ -43,6 +62,47 @@ sub decode_word ($word) {
 sub decode ($unmessage) {
    my @words = map decode_word($_), split /\s/, $unmessage;
    return join q{ }, @words;
+}
+
+my $ob_mapping = {
+   a => 'ठ',
+   b => 'अ',
+   c => 'इ',
+   d => 'उ',
+   e => 'ऋ',
+   f => 'ए',
+   g => 'क',
+   h => 'ख',
+   i => 'ग',
+   j => 'घ',
+   k => 'ङ',
+   l => 'च',
+   m => 'छ',
+   n => 'ज',
+   o => 'झ',
+   p => 'ञ',
+   q => 'ट',
+   r => 'ब',
+   s => 'ड',
+   t => 'ढ',
+   u => 'ण',
+   v => 'त',
+   w => 'थ',
+   x => 'द',
+   y => 'ध',
+   z => 'न',
+};
+
+my @keys = keys %{$ob_mapping};
+my @values = values %{$ob_mapping};
+my $unob_mapping = { zip @values, @keys };
+
+sub orbfuscate ($message) {
+   return tr_with_mapping $ob_mapping, $message;
+}
+
+sub unorbfuscate ($message) {
+   return tr_with_mapping $unob_mapping, $message;
 }
 
 "for catherine";
--- t/02-code.t
+++ t/02-code.t
@@ -5,10 +5,18 @@ use warnings;
 use Test::More 'no_plan';
 
 use Crypt::NQR13_f;
+use utf8;
 
 my $message = 'a frew is weird';
-my $encoded = Crypt::NQR13_f::encode($message);
+my ($encoded, $mapping) = Crypt::NQR13_f::encode($message);
 like $encoded, qr/XaZ w[er]{2}f sYi d[eir]{3}w/, 'encoding works';
 
 $message = Crypt::NQR13_f::decode($encoded);
 is $message, 'a ferw is weird',  'decoding works';
+my $mapping = Crypt::NQR13_f::generate_mapping;
+my $tr_frew = Crypt::NQR13_f::tr_with_mapping($mapping, 'frew');
+my $untr_frew = Crypt::NQR13_f::untr_with_mapping($mapping, $tr_frew);
+cmp_ok $tr_frew, 'ne', $untr_frew, q{tr'd strings are different};
+is $untr_frew, 'frew', 'tr goes both ways correctly';
+my $obd = Crypt::NQR13_f::orbfuscate('this is a complex mapping!');
+is Crypt::NQR13_f::unorbfuscate($obd), 'this is a complex mapping!', 'unob works';
